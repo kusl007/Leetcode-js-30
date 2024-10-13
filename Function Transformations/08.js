19903bf465025a7bf9da12b72c2ae411aa017775
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-10-13 15:53:13 +0545

    day 8 completed

--- Function Transformations/08.js
+++ Function Transformations/08.js
@@ -1,12 +1,14 @@
 //leetcode  2629. Function Composition
 
+
 var compose = function(functions) {
     
     return function(x) {
-        for(let i=functions.length-1;i>=0;i++){
-           x =function[i](x);
+        for(let i=functions.length-1;i>=0;i--){
+           x =functions[i](x);
         }
         return x;
         
     }
 };
+
